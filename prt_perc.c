9599ad22928cb58bc9bcec4b51370c5aead77e3e
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-10-29 11:32:47 +0000

    [Feature]: Prints a percentage if it finds 2 of them prt_perc.c

---
+++ prt_perc.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * prt_perc - prints a percentage
+ * @valist: arguments
+ * Return: 1 on success
+ */
+int prt_perc(va_list valist)
+{
+	(void) valist;
+	_putchar('%');
+	return (1);
+}
